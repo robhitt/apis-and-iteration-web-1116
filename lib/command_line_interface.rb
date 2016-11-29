f4791c9cbe9a3896350baf45d52472e8aab389ec
Rob Hitt <robhitt@gmail.com>
2016-11-29 11:51:16 -0500

    gets user input

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the star wars film generator"
 end
 
 def get_character_from_user
-  puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  puts "please enter a character (use the force)"
+  character = gets.chomp.downcase
 end
